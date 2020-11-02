ccf6cc0911580e92638833f15244d8666cf9a8eb
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2020-11-02 20:51:01 -0300

    Create Q09.py

---
+++ Q09.py
@@ -0,0 +1,11 @@
+$limiteInferior = intval(readline('Valor do limite inferior: '));
+$limiteSuperior = intval(readline('Valor do limite superior: '));
+
+
+
+for ($i=$limiteInferior; $i <= $limiteSuperior; $i++) { 
+    if($i % 2 !== 0){   
+        echo "Valor Ímpar : $i\n";
+    }
+    
+}

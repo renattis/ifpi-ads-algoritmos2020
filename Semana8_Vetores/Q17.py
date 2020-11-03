eedf5975fda8aea4de11ae6ee502fe45af99464b
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2020-11-03 00:46:15 -0300

    Create Q17.py

---
+++ Semana8_Vetores/Q17.py
@@ -0,0 +1,32 @@
+
+N = int(input('N: '))
+matriz = []
+
+for i in range(N):
+    matriz.append([])
+    for j in range(N):
+        matriz[i].append( int( input('Valor: ') ) )
+
+def maior_linha_em( matriz ):
+    maior_linha = 0
+    maior_soma = sum(matriz[maior_linha])
+    for i in range(N):
+        sumLine = sum( matriz[i] )
+        if( sumLine > maior_soma ):
+            maior_soma = sumLine
+            maior_linha = i
+    return maior_linha
+
+def menor_linha_em( matriz ):
+    menor_linha = 0
+    menor_soma = sum(matriz[0])
+    for i in range(N):
+        sumLine = sum( matriz[i] )
+        if( sumLine < menor_soma ):
+            menor_soma = sumLine
+            menor_linha = i
+    return menor_linha
+
+print('matriz', matriz)
+print('maior soma é da linha:', maior_linha_em(matriz) + 1 )
+print('menor soma é da linha:', menor_linha_em(matriz) + 1 )

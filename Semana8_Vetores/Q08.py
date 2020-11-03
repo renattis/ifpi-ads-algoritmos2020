586ad734e6d3c218db454747298e95622dbaae5a
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2020-11-03 00:42:24 -0300

    Create Q08.py

---
+++ Semana8_Vetores/Q08.py
@@ -0,0 +1,22 @@
+N = int(input('N: '))
+vetor = []
+for i in range(N):vetor.append( int( input('Valor: ') ) )
+
+def bigger_element( vetor ):
+    bigger = vetor[0]
+    for n in vetor:       
+        if( n > bigger ):
+            bigger = n
+
+    return bigger
+
+def smaller_element( vetor ):
+    smaller = vetor[0]
+    for n in vetor:     
+        if( n < smaller ):
+            smaller = n
+
+    return smaller
+
+print('maior:', bigger_element( vetor ) )
+print('menor:', smaller_element( vetor ) )

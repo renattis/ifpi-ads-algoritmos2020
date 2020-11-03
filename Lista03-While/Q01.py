9875c10dbafef83bfd44c238dcf1608c6352536e
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2020-11-02 22:32:38 -0300

    Create Q01.py

---
+++ Lista03-While/Q01.py
@@ -0,0 +1,7 @@
+numeros = int(input('Informe um número inteiro: '))
+print(f'Os números inteiros do intervalo de 1 a {numeros} são:', end=' ')
+i = 1
+while i <= numeros: 
+    print(i, end=' ')
+    i += 1
+print('')

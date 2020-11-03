191ab4a8dce6ee54f0baa0941aaea87ca7aabc75
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2020-11-03 01:11:57 -0300

    Create Strongpassword.py

---
+++ semana4_Strings/Strongpassword.py
@@ -0,0 +1,16 @@
+senha = input('Digite sua senha: ')
+tamanho = len(senha)
+
+tamanho >= 6
+
+# especiais
+especials = '!','@','#','$','%','^','&','*','(',')','-','+'
+
+#maiúsculo
+range(65,91)
+
+# minúsculo
+range(97,123)
+
+# números
+range (48,57)

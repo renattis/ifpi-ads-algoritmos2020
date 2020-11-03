3f5740a7356d2f692908bd062881d6beade6d664
Pedro Felipe Barros <73582559+renattis@users.noreply.github.com>
2020-11-02 21:55:28 -0300

    Create Q14.py

---
+++ Lista03/Q14.py
@@ -0,0 +1,16 @@
+
+    $N = intval(readline('Valor de N: '));
+    
+    for ($i = ($N - 1); $i >= 1; $i--) {    
+        if( sqrt($i) === floor(sqrt($i)) ){
+            echo "Maior quadrado antecessor -> $i\n";
+            break;
+        }
+    }
+    
+    
+    
+    
+    
+
+?>
